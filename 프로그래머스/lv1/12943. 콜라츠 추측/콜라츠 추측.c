2738f3971f319bee4e9568ab74d04c92d0bff0a7
Seonggyu-Bae <45026759+Seonggyu-Bae@users.noreply.github.com>
2023-03-19 18:14:41 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num) {
+    int answer = 0;
+    
+    if(num==0)
+        return 0;
+    
+    while(num!=1)
+    {
+        
+        if(num%2==1)
+            num = num*3 +1;
+        else
+            num/=2;
+        
+        answer++;
+        if(answer==500)
+            return -1;
+    }
+    
+    return answer;
+}
