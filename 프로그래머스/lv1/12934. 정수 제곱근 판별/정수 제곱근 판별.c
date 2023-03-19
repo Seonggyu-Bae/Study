c902c8ecd4c1d5a2f4180ab90c0650814c37d787
Seonggyu-Bae <45026759+Seonggyu-Bae@users.noreply.github.com>
2023-03-19 18:14:19 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+long long solution(long long n) {
+    long long answer = -1;
+    
+    for(long long i=1; i*i<=n; i++ ){
+        if(i*i==n)
+            return (i+1) * (i+1);
+    }
+    
+    
+    return answer;
+}
