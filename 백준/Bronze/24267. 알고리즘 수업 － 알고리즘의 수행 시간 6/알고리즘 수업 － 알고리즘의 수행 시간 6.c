8991486d3b026dab5edddc4c559378e244316878
Seonggyu-Bae <45026759+Seonggyu-Bae@users.noreply.github.com>
2023-03-28 16:28:31 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+
+	unsigned long long n;
+	scanf("%d", &n);
+
+	printf("%lld\n3", n*(n-2)*(n-1)/6);
+
+    return 0;
+}
