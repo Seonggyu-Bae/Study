afc764a4aef29baae5e2dc6b6580ed68a9ea70bf
Seonggyu-Bae <45026759+Seonggyu-Bae@users.noreply.github.com>
2023-04-11 23:05:42 +0900

    [Bronze II] Title: 진법 변환, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.c
+++ 백준/Bronze/2745. 진법 변환/진법 변환.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 int main() {
 
 	char N[100];
 	int B;
 	int sum = 0;
+	int power = 1;
 	scanf("%s %d", &N, &B);
 
-	int len = strlen(N);
-	
-	for (int i = 0; i < len; i++) 
+	for (int i = strlen(N)-1 ; i >= 0; i--) 
 	{
 		if (N[i] >= '0' && N[i] <= '9')
-			sum += pow(B, len - i - 1) * (N[i] - '0');
+			sum += power * (N[i] - '0');
 		else
-			sum += pow(B, len - i - 1) * (N[i] - 55);
+			sum += power * (N[i] - 55);
+
+		power *= B;
 	}
 
 	printf("%d",sum);
