8f02915121150e1f51ef8f1f2f358c4c43b6967a
Seonggyu-Bae <45026759+Seonggyu-Bae@users.noreply.github.com>
2023-05-10 18:43:19 +0900

    [Silver IV] Title: 수들의 합 2, Time: 28 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main() {
+
+    int N=0, M=0;
+    int sum = 0;
+    int count = 0;
+    scanf("%d %d", &N, &M);
+    int A[10000] = { 0, };
+
+    for (int i = 0; i < N; i++)
+        scanf("%d", &A[i]);
+
+    for (int i = 0; i < N; i++) {
+        sum = 0;
+        for (int j = i; j < N; j++) {
+            sum += A[j];
+            if (sum == M) {
+                count++;
+                sum = 0;
+                break;
+            }
+        }
+    }
+    
+    printf("%d", count);
+
+    return 0;
+}
