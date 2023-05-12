c0abf21e8ab9b1796a53f850b92a86976ca5dba0
Seonggyu-Bae <45026759+Seonggyu-Bae@users.noreply.github.com>
2023-05-12 12:16:29 +0900

    [Bronze I] Title: 진법 변환 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void reverse(char* num) {
+    int len, temp;
+    len = strlen(num);
+
+    for (int i = 0; i < len / 2; i++) {
+        temp = num[i];
+        num[i] = num[len - i - 1];
+        num[len - i - 1] = temp;
+    }
+
+}
+int main() {
+    int N, B;
+    int i = 0;
+    scanf("%d %d", &N, &B);
+    char ans[100] = { 0, };
+
+    if (B > 10) {
+        while (N > 0) {
+            if (N % B > 9)
+                ans[i] = (N % B) + 55;
+            else
+                ans[i] = (N % B) + 48;
+
+            i++;
+            N /= B;
+        }
+        reverse(ans);
+    }
+    else if (B == 10) {
+        printf("%d", N);
+        return 0;
+    }
+    else {
+        while (N > 0) {
+            if (N % B > 9)
+                ans[i] = (N % B) + 55;
+            else
+                ans[i] = (N % B) + 48;
+
+            i++;
+            N /= B;
+        }
+        reverse(ans);
+    }
+
+    printf("%s", ans);
+   
+    return 0;
+}
