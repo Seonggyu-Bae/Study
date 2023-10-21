7499e1efcd28b1d7d2f846fa6a7597fd7351ebb2
Seonggyu-Bae <45026759+Seonggyu-Bae@users.noreply.github.com>
2023-10-21 22:24:08 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.py
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.py
@@ -2,12 +2,14 @@ import sys
 
 N = int(input())
 
-my_set = set(('ChongChong',))
+my_set = set()
+my_set.add('ChongChong')
 
 for _ in range(N):
     A, B = sys.stdin.readline().rstrip().split()
-    if A in my_set or B in my_set:
-        my_set.add(A)
+    if A in my_set:
         my_set.add(B)
+    elif B in my_set:
+        my_set.add(A)
 
 print(len(my_set))
