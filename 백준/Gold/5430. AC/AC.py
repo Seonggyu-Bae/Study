775692104c0d9557fa2c6016fc70407a8a6c75d0
Seonggyu-Bae <45026759+Seonggyu-Bae@users.noreply.github.com>
2023-10-24 21:13:23 +0900

    [Gold V] Title: AC, Time: 3036 ms, Memory: 39932 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.py
@@ -0,0 +1,63 @@
+from collections import deque
+from sys import stdin
+
+
+def solve():
+    is_reverse = False
+    for f in p:
+        if f == 'R':
+            if is_reverse:
+                is_reverse = False
+            else:
+                is_reverse = True
+        else:
+            if num:
+                if is_reverse:
+                    num.pop()
+                else:
+                    num.popleft()
+            else:
+                return False
+    if is_reverse:
+        num.reverse()
+    return True
+
+
+T = int(stdin.readline().rstrip())
+
+for tc in range(T):
+    func = stdin.readline().rstrip()
+    n_len = int(stdin.readline().rstrip())
+    x = stdin.readline().rstrip()
+    num = deque()
+    if n_len > 0:
+        num = deque(map(int, x[1:-1].split(',')))
+    p = ''
+    count = 0
+    for f in func:
+        if f == 'R':
+            count += 1
+        else:
+            if count % 2 == 0:
+                p += 'D'
+            else:
+                p += 'RD'
+            count = 0
+    if count % 2 != 0:
+        p += 'R'
+
+    if solve():
+        if num:
+            ans = '['
+            for idx,x in enumerate(num):
+                if idx == len(num) -1:
+                    ans += str(x)
+                else:
+                    ans += str(x)
+                    ans += ','
+            ans +=']'
+            print(ans)
+        else:
+            print('[]')
+    else:
+        print('error')
