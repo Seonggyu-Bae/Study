15359705b535f814ea530811982b37da7098f80a
Seonggyu-Bae <45026759+Seonggyu-Bae@users.noreply.github.com>
2023-10-24 21:26:00 +0900

    [Gold V] Title: AC, Time: 128 ms, Memory: 42876 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.py
+++ 백준/Gold/5430. AC/AC.py
@@ -3,6 +3,10 @@ from sys import stdin
 
 
 def solve():
+    # 함수 'R'이 나오면 뒤집어야 하지만
+    # 뒤집지 않고 표시만 해둠 -> 연산을 줄여 시간복잡도를 낮춤
+    # 'D'가 나오면 뒤집혀있으면 맨 뒷쪽 숫자를 지우고
+    # 정방향이라면 맨 앞쪽 숫자를 지우면된다
     is_reverse = False
     for f in func:
         if f == 'R':
@@ -28,6 +32,7 @@ for tc in range(T):
     x = stdin.readline().rstrip()
 
     if n_len:
+        # 슬라이싱과 split을 이용해서 함수 사용을 쉽게 만들어줌
         x = x[1:-1].split(',')
     else:
         x = []
